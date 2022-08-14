e04ee76b5218659c9f0a1d6db60d294e8762df69
Vasili Turtugeshev <vasia19rus@bk.ru>
2022-08-14 22:05:25 +0700

    learn get and query parameters

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
 
+from typing import Optional
+
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"data": {"name": 'TROLOLO'}}
+@app.get("/blog")
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f"published {limit} show db"}
+    else:
+        return {"data": f"NOT published {limit} show db"}
 
 
-@app.get("/about")
-def about():
-    return "it's about me"
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "unpublished blog"}
+
+
+@app.get("/blog/{blog_id}")
+def about(blog_id: int):
+    return {"data": 'ads'}
+
+
+@app.get("/blog/{blog_id}/comments")
+def comments():
+    return {"data": {"1", "2"}}
